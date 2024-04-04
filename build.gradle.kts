076a86a51f0151df54711318b6a46d8399b2d650
Kevin <kevin@turuntururun.com>
2024-04-03 22:12:58 -0600

    added main

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,3 +54,7 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+tasks.withType<Jar> {
+    manifest.attributes["Main-Class"] = "com.turuntururun.datamuncher.DataMuncherApplication"
+}
